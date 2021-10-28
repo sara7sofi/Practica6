b1f374bcbdcc7cf0d86a12ac77a09b40c5c1bc5a
sara7sofi <sara.arias.7@gmail.com>
2021-10-28 16:23:41 +0200

    Segundo cambio

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,7 +1,8 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println( "¡Hola, Sara! ");
-        System.out.println( "¡Hola, Rebeca! "); //Este es el primer cambio
+        System.out.println( "¡Hola, Rebeca! ");//Este es el primer cambio
+        System.out.println( "¡Hola, a todos ");// Este es el segundo cambio
 
     }
 }
